6ce2f2686e4dfce3b2fb2ad7c4a183592343dc7a
ksh04023 <ksh04023@gmail.com>
2022-07-07 23:08:56 +0900

    'initial'

---
+++ app/src/main/java/com/android/alarmwithwidget/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.android.alarmwithwidget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import java.util.stream.DoubleStream.builder
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        init()
+    }
+    fun init(){
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+}
---
+++ app/src/main/java/com/android/alarmwithwidget/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.android.alarmwithwidget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+//Google login Client Id: 1082854400849-4roo9cssbpte3323sc04bqak1b284i3b.apps.googleusercontent.com
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkPermission()
+    }
+
+    fun checkPermission(){
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+    }
+}
